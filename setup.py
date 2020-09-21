4cff15ef31b057fa05dbbbd3a2832546f828acd2
staumoepeau <sione.taumoepeau@gmail.com>
2020-09-21 15:14:53 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+import re, ast
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
+#with open('requirements.txt') as f:
+#	install_requires = f.read().strip().split('\n')
 
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('nextpivot/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+	
 # get version from __version__ variable in nextpivot/__init__.py
-from nextpivot import __version__ as version
 
 setup(
 	name='nextpivot',
@@ -16,5 +22,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+#	install_requires=install_requires
 )
